31a41826704844ea00a6c5faad7317938d6c1d25
Josiah Coad <josiah@coad.net>
2018-04-06 00:04:43 -0500

    ok

--- addWeeks.py
+++ addWeeks.py
@@ -1,11 +1,14 @@
-
+# pylint: skip-file
 import pandas as pd
 
+
 def addWeeks(df):
-	df['datetime_values'] = df['Trip Start Timestamp'].map(lambda x: pd.to_datetime(x))
+	df['datetime_values'] = df['Trip Start Timestamp'].map(
+		lambda x: pd.to_datetime(x))
 	df['week'] = df['datetime_values'].dt.week
-	return df
+    return df
+
 
 if __name__ == "__main__":
-	df = pd.read_csv('Chicago_taxi_trips2017.csv', nrows = 1000000)
-	df = addWeeks(df)
+    df = pd.read_csv('Chicago_taxi_trips2017.csv', nrows=1000000)
+    df = addWeeks(df)
--- calculateSums.py
+++ calculateSums.py
@@ -1,11 +1,18 @@
 # pylint: skip-file
 from addWeeks import addWeeks
 import pandas as pd
+import os
+import subprocess
 
 
-def sumsByTaxiID(column, df):
+import sys
+
+filedir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(filedir, os.pardir))
+os.chdir(filedir)
+
+def sumsByTaxiID(column, df, trip_miles):
     weeks = range(1, 54)
-    trip_miles = {}
     for week in weeks:
         df_week = df[(df['week'] == week)]
         for _, val in df_week[['Taxi ID', column]].iterrows():
@@ -14,12 +21,25 @@ def sumsByTaxiID(column, df):
             else:
                 trip_miles[val[0]][week - 1] += val[1]
     # trip_miles # {taxiId1: [sum-week1, ... , sum-week53], ... , taxiIdN: [sum-week1, ... , sum-week53]}
+    return trip_miles
+
+
+def readAllRows(filename, step, column):
+    test = subprocess.Popen(["wc", "-l", filename], stdout=subprocess.PIPE)
+    # output = test.communicate()[0]
+    # total = int(str(output).split()[1])
+    total = 1000
+    trip_miles = {}
+    for rowNum in range(0, total, step):
+        df = pd.read_csv(filename, skiprows=rowNum, nrows=step)
+        df = addWeeks(df)
+        trip_miles = sumsByTaxiID(column, df, trip_miles)
     headers = ['Taxi ID', *[f'week{i}' for i in range(1, 54)]]
-    return pd.DataFrame([[key, *val] for key, val in trip_miles.items()], columns=headers)
+    return pd.DataFrame([[key, *val] for key, val in trip_miles.items()], columns=headers, index=False)
 
 
 if __name__ == "__main__":
-    df = pd.read_csv('../data/Chicago_taxi_trips2017.csv', nrows=10000)
-    df = addWeeks(df)
-    result = sumsByTaxiID("Trip Miles", df)
+
+    result = readAllRows(
+        "../data/Chicago_taxi_trips2017.csv", 100, "Trip Miles")
     result.to_csv("out.csv")
