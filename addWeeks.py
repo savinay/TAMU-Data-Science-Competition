5a4c47e18a4e31a2ebf3b8d324597de9b01e8ec6
Josiah Coad <josiah@coad.net>
2018-04-06 01:06:39 -0500

    tikae

--- addWeeks.py
+++ addWeeks.py
@@ -3,9 +3,11 @@ import pandas as pd
 
 
 def addWeeks(df):
-	df['datetime_values'] = df['Trip Start Timestamp'].map(
-		lambda x: pd.to_datetime(x))
-	df['week'] = df['datetime_values'].dt.week
+    if 'Trip Start Timestamp' in df:
+        df['datetime_values'] = df['Trip Start Timestamp'].map(lambda x: pd.to_datetime(x))
+    else:
+        print(df)
+    df['week'] = df['datetime_values'].dt.week
     return df
 
 
--- calculateSums.py
+++ calculateSums.py
@@ -11,6 +11,7 @@ filedir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(filedir, os.pardir))
 os.chdir(filedir)
 
+
 def sumsByTaxiID(column, df, trip_miles):
     weeks = range(1, 54)
     for week in weeks:
@@ -28,18 +29,24 @@ def readAllRows(filename, step, column):
     test = subprocess.Popen(["wc", "-l", filename], stdout=subprocess.PIPE)
     # output = test.communicate()[0]
     # total = int(str(output).split()[1])
-    total = 1000
+    total = 100000
     trip_miles = {}
     for rowNum in range(0, total, step):
-        df = pd.read_csv(filename, skiprows=rowNum, nrows=step)
+        df = pd.read_csv("../data/Chicago_taxi_trips2017.csv",
+                        skiprows=range(1, rowNum), nrows=step, 
+                        usecols=["Taxi ID", "Trip Miles", "Trip Start Timestamp"],
+                        dtype={
+                            "Taxi ID": object,
+                            "Trip Miles": float,
+                            "Trip Start Timestamp": object
+                        })
         df = addWeeks(df)
         trip_miles = sumsByTaxiID(column, df, trip_miles)
     headers = ['Taxi ID', *[f'week{i}' for i in range(1, 54)]]
-    return pd.DataFrame([[key, *val] for key, val in trip_miles.items()], columns=headers, index=False)
+    return pd.DataFrame([[key, *val] for key, val in trip_miles.items()], columns=headers, index=None)
 
 
 if __name__ == "__main__":
-
     result = readAllRows(
-        "../data/Chicago_taxi_trips2017.csv", 100, "Trip Miles")
-    result.to_csv("out.csv")
+        "../data/Chicago_taxi_trips2017.csv", 10000, "Trip Miles")
+    result.to_csv("out2.csv", index=False)
