347b3a3f56ad57755e75e73933039f7e5014fb70
savinay <savinay.90@gmail.com>
2018-04-07 00:46:15 -0500

    removed zeros from calculation of median

--- calculateMedian.py
+++ calculateMedian.py
@@ -2,18 +2,22 @@ import pandas as pd
 import statistics
 import numpy as np
 
-def calculateMedian():
-    df = pd.read_csv("out.csv")
+def calculateMedian(filename):
+    df = pd.read_csv(filename)
     weeks = range(1,54)
     median = []
     for week in weeks:
         data = df['week'+str(week)]
-        median.append(statistics.median(data.values))
-    headers = [f'week{i}' for i in range(1, 54)]
+        values = [x for x in data.values if x != 0]
+        print(values)
+        if len(values) > 0:
+            median.append(statistics.median(values))
+
+    headers = [f'week{i}' for i in range(1, 1+len(median))]
     return pd.DataFrame(np.array(median).reshape(-1, len(median)), columns=headers, index=None)
 
 if __name__ == "__main__":
-    res = calculateMedian()
-    res.to_csv("trip_miles_median.csv", index=False)
+    res = calculateMedian("trip_miles_2015.csv")
+    res.to_csv("trip_miles_median_2015.csv", index=False)
         
 
