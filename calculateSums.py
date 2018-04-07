0650b51fdf4e2426448736d1517965c01c5cc3f4
Josiah Coad <josiah@coad.net>
2018-04-07 11:51:57 -0500

    progress

--- calculateSums.py
+++ calculateSums.py
@@ -13,6 +13,9 @@ os.chdir(filedir)
 
 
 def getwknum(string):
+    # this could be sped up since split is O(n)
+    # if we could work under assumption timestamps are all exact same,
+    # then we can split on index
     month, day, year = map(int, string.split()[0].split("/"))
     return dt.datetime(year, month, day, 0, 0, 0).timetuple().tm_yday // 7
 
@@ -29,15 +32,28 @@ def sumsByTaxiID(column, new_df):
     return k
 
 
-def readAllRows(filename, chunksize, column):
-    # test = subprocess.Popen(["wc", "-l", filename], stdout=subprocess.PIPE)
-    # output = test.communicate()[0]
-    # total = int(str(output).split()[1])
-    total = 100000
+def convert(data):
+    trip_miles = {}
+    for k, v in data.items():
+        taxiID = k[0]
+        if taxiID not in trip_miles:
+            trip_miles[taxiID] = [0] * 53
+        wknum = k[1]
+        trip_miles[taxiID][wknum] += float(v)
+    return trip_miles
+
+
+def readAllRows(filename, chunksize, column, nrows=None):
+    if not nrows:
+        print(f"Reading total number of lines in {filename}.")
+        test = subprocess.Popen(["wc", "-l", filename], stdout=subprocess.PIPE)
+        output = test.communicate()[0]
+        nrows = int(str(output).split()[1])
+        print(f"{nrows} read.")
     total_count = Counter()
-    print("Start.")
+    print("Start reading.")
     t0 = time.time()
-    start = time.time()
+    starttime = time.time()
     count = 1
     for df in pd.read_csv(filename,
                           usecols=["Taxi ID", "Trip Miles",
@@ -49,22 +65,20 @@ def readAllRows(filename, chunksize, column):
                           },
                           chunksize=chunksize,
                           iterator=True,
-                          nrows=total):
+                          nrows=nrows):
         total_count += sumsByTaxiID(column, addWeeks(df))
         t1 = time.time()
         # print(f"Time for this loop is {t1 - t0} and average {(t1 - start) / count}")
         print(f"Rows: {chunksize * count}")
         count += 1
         t0 = t1
-    print(f"Done in total time {t1 - start}")
+    print(f"Done in total time {(t1 - starttime) / 60} min.")
     headers = ['Taxi ID', *[f'week{i}' for i in range(1, 54)]]
-    # return pd.DataFrame([[key, *val] for key, val in total_df.items()], columns=headers, index=None)
-    return total_count
+    return pd.DataFrame([[key, *val] for key, val in convert(total_count).items()], columns=headers, index=None)
 
 
 if __name__ == "__main__":
     result = readAllRows(
-        "Chicago_taxi_trips2017.csv", 10000, "Trip Miles")
-    # print(result)
-    # result.to_csv("out.csv", index=False)
-    # get num of taxis: cars['Manufacturer'].nunique()
+        "testdata.csv", 1, "Trip Miles")
+    result.to_csv("testout.csv", index=False)
+    # get num of taxis: df['Taxi ID'].nunique()
