30f71d1002c39abb2d1e8ddba1a55d7ea46b0371
Josiah Coad <josiah@coad.net>
2018-04-08 23:41:57 -0500

    updates

--- addWeeksToAll.py
+++ addWeeksToAll.py
@@ -1,3 +1,4 @@
+# pylint: skip-file
 import pandas as pd
 from collections import Counter
 import numpy as np
@@ -5,9 +6,6 @@ import datetime as dt
 import time
 
 def getwknum(string):
-    # this could be sped up since split is O(n)
-    # if we could work under assumption timestamps are all exact same,
-    # then we can split on index
     month, day, year = map(int, string.split()[0].split("/"))
     return dt.datetime(year, month, day, 0, 0, 0).timetuple().tm_yday // 7
 
@@ -16,25 +14,24 @@ def addWeeks(df):
     df['week'] = df['Trip Start Timestamp'].transform(lambda x: getwknum(x))
     return df
 
-for i in range(5, 6):
-    filename = f"Chicago_taxi_trips201{i}.csv"
-    # for column in ["Trip Total", "Trip Seconds", "Tolls", "Fare", "Tips", "Tolls", "Extras"]:
-    df = pd.read_csv(filename,
-                    usecols=[ "Taxi ID", "Trip Start Timestamp", "Trip Total", "Trip Seconds", "Tolls", "Fare",              "Tips", "Tolls", "Extras", "Pickup Centroid Latitude", "Pickup Centroid Longitude",            "Dropoff Centroid Latitude", "Dropoff Centroid Longitude"],
-                    dtype={
-                        "Taxi ID": object,
-                        "Trip Start Timestamp": object,
-                        "Trip Total": object, 
-                        "Trip Seconds": float,
-                        "Tolls": object,
-                        "Fare": object,
-                        "Tips": object,
-                        "Tolls": object,
-                        "Extras": object,
-                        "Pickup Centroid Latitude": float,
-                        "Pickup Centroid Longitude": float,
-                        "Dropoff Centroid Latitude": float,
-                        "Dropoff Centroid Longitude": float
-                    })
-    df = addWeeks(df)
-    df.to_csv(filename, index=False)
+
+filename = "Chicago_taxi_trips2014.csv"
+df = pd.read_csv(filename,
+                usecols=[ "Taxi ID", "Trip Start Timestamp", "Trip Total", "Trip Seconds", "Tolls", "Fare",              "Tips", "Tolls", "Extras", "Pickup Centroid Latitude", "Pickup Centroid Longitude",            "Dropoff Centroid Latitude", "Dropoff Centroid Longitude"],
+                dtype={
+                    "Taxi ID": object,
+                    "Trip Start Timestamp": object,
+                    "Trip Total": object, 
+                    "Trip Seconds": float,
+                    "Tolls": object,
+                    "Fare": object,
+                    "Tips": object,
+                    "Tolls": object,
+                    "Extras": object,
+                    "Pickup Centroid Latitude": float,
+                    "Pickup Centroid Longitude": float,
+                    "Dropoff Centroid Latitude": float,
+                    "Dropoff Centroid Longitude": float
+                })
+df = addWeeks(df)
+df.to_csv(filename, index=False)
--- calculateSumsNew.py
+++ calculateSumsNew.py
@@ -1,9 +1,20 @@
 # pylint: skip-file
 import pandas as pd
-from collections import Counter
-import numpy as np
-import datetime as dt
 import time
+import datetime as dt
+
+def getwknum(string):
+    month, day, year = map(int, string.split()[0].split("/"))
+    return dt.datetime(year, month, day, 0, 0, 0).timetuple().tm_yday // 7
+
+
+def addWeeks(df):
+    df['week'] = df['Trip Start Timestamp'].transform(lambda x: getwknum(x))
+    return df
+
+
+def sumsByTaxiID(column, new_df):
+    return new_df.groupby(["Taxi ID", "week"])[column].sum()
 
 
 def convert(data):
@@ -19,16 +30,19 @@ def convert(data):
 
 def getSums(filename, column):
     t0 = time.time()
+    print("Start.")
     df = pd.read_csv("Chicago_taxi_trips2015.csv",
                      usecols=["Taxi ID", column, "Trip Start Timestamp"],
                      dtype={
                          "Taxi ID": object,
-                         column: float,
+                         column: object,
                          "Trip Start Timestamp": object
                      })
     t1 = time.time()
+    # print(type(df[column]))
+    df[column] = df[column].map(lambda x: x if type(x) == float else float(x[1:]))
     print(f"{filename} done in {t1-t0} sec.")
-    total_count = df.groupby(["Taxi ID", "week"])[column].sum().to_dict()
+    total_count = addWeeks(df).groupby(["Taxi ID", "week"])[column].sum().to_dict()
     headers = ['Taxi ID', *[f'week{i}' for i in range(1, 54)]]
     result = pd.DataFrame(
         [[key, *val] for key, val in convert(total_count).items()], columns=headers, index=None)
@@ -36,7 +50,5 @@ def getSums(filename, column):
     print(f"{filename}_{column}_sums.csv written.")
 
 if __name__ == "__main__":
-    for i in range(3, 7):
-        filename = f"Chicago_taxi_trips201{i}.csv"
-        for column in ["Trip Total", "Trip Seconds", "Tolls", "Fare", "Tips", "Tolls", "Extras"]:
-            getSums(filename, column)
+    filename = "Chicago_taxi_trips2015.csv"
+    getSums(filename, "Trip Total")
