b74c18faf85a304194842dd37fd573fe9f9da3e1
savinay <savinay.90@gmail.com>
2018-04-08 23:12:55 -0500

    progress

--- calculateSumsNew.py
+++ calculateSumsNew.py
@@ -20,15 +20,18 @@ def convert(data):
 def getSums(filename, column, dictionary):
     t0 = time.time()
     df = pd.read_csv(filename,
-                     usecols=["Taxi ID", column, "Trip Start Timestamp"],
+                     usecols=["Taxi ID", column, "week"],
                      dtype={
                          "Taxi ID": object,
                          column: dictionary[column],
-                         "Trip Start Timestamp": object
+                         "week": int
                      })
     t1 = time.time()
     print(f"{filename} done in {t1-t0} sec.")
-    total_count = df.groupby(["Taxi ID", "week"])[column].sum().to_dict()
+    if dictionary[column] == object:
+        total_count = df.groupby(["Taxi ID", "week"])[column].transform(lambda x: float(x[1:])).sum().to_dict()
+    else:
+        total_count = df.groupby(["Taxi ID", "week"])[column].sum().to_dict()
     headers = ['Taxi ID', *[f'week{i}' for i in range(1, 54)]]
     result = pd.DataFrame(
         [[key, *val] for key, val in convert(total_count).items()], columns=headers, index=None)
