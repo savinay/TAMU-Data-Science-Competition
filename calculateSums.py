6dfd0e7742dda85e42365b4cd60e3d2f53dcba2a
Josiah Coad <josiah@coad.net>
2018-04-15 16:43:41 -0500

    update

--- calculateSums.py
+++ calculateSums.py
@@ -1,25 +1,40 @@
-# pylint: skip-file
+# pylint: disable=C0013
 """Get sums of things like trip totals and trip seconds."""
 
 import datetime as dt
 import time
-
+from multiprocessing import Pool
+import numpy as np
 import pandas as pd
+
 
 HEADERS = ['Taxi ID', *[f'week{i}' for i in range(1, 54)]]
 DATATYPES = {
     "Taxi ID": object, "Trip Start Timestamp": object,
     "Trip Miles": float, "Trip Total": object, "Trip Seconds": float,
-    "Tolls": object, "Fare": object, "Tips": object, "Tolls": object, "Extras": object,
-    "Pickup Centroid Latitude": float, "Pickup Centroid Longitude": float,
-    "Dropoff Centroid Latitude": float, "Dropoff Centroid Longitude": float,
-    "Pickup Centroid Location": object
+    "Tolls": object, "Fare": object, "Tips": object, "Extras": object
 }
 
 
-def getwknum(string):
+def parallelize_dataframe(df, func):
+    num_partitions = 100
+    num_cores = 4
+    df_split = np.array_split(df, num_partitions)
+    pool = Pool(num_cores)
+    df = pd.concat(pool.map(func, df_split))
+    pool.close()
+    pool.join()
+    return df
+
+
+def getdaynum(string):
     month, day, year = map(int, [string[:2], string[2:4], string[6:]])
-    return dt.datetime(year, month, day, 0, 0, 0).timetuple().tm_yday // 7
+    return dt.datetime(year, month, day, 0, 0, 0).timetuple().tm_yday
+
+
+def addDays(df):
+    df["day"] = df["Trip Start Timestamp"].map(getdaynum)
+    return df
 
 
 def getSums(column, df):
@@ -37,17 +52,20 @@ def getSums(column, df):
     return total_count
 
 
-def readWrite(year):
-    datapath = "/Users/josiahcoad/Desktop/Coding/dataScienceComp2017/TAMU-Data-Science-Competition/original/"
-    filename = f"{datapath}Chicago_taxi_trips{year}_weeks.csv"
+def readWrite(datapath, year):
+    filename = f"{datapath}Chicago_taxi_trips{year}.csv"
     t0 = time.time()
-    readcols = ["Trip Total", "Trip Miles"]
+    readcols = ["Trip Total", "Trip Miles",
+                "Trip Seconds", "Fare", "Tolls", "Extras", "Tips"]
     df = pd.read_csv(filename,
                      usecols=readcols +
-                     ["Taxi ID", "week"],
+                     ["Taxi ID", "Trip Start Timestamp"],
                      dtype=DATATYPES,
                      nrows=10000)
     print(f"{filename} read in {time.time()-t0} sec.")
+
+    df = parallelize_dataframe(df, addWeeks)
+    print(f"Weeks added in {round(time.time()-t0)} sec.")
 
     medians = pd.DataFrame()
     for column in readcols:
@@ -60,4 +78,5 @@ def readWrite(year):
 
 
 if __name__ == "__main__":
-    readWrite(2013)
+    datapath = "/Users/josiahcoad/Desktop/Coding/dataScienceComp2017/TAMU-Data-Science-Competition/original/"
+    readWrite(datapath, 2013)
