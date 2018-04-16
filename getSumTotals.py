a4f00ffc0379a74985d4b9e30ccde1ffd354b8fa
Josiah Coad <josiah@coad.net>
2018-04-15 19:24:51 -0500

    update

--- getSumTotals.py
+++ getSumTotals.py
@@ -1,17 +1,17 @@
 # pylint: disable=missing-docstring, invalid-name
-"""Get weekly/daily/hourly sums per taxi and medians across all taxis
-of things like trip totals and trip seconds."""
+"""Get weekly/daily/hourly total sums for a given timechunk."""
 
 import os
 import pandas as pd
 
 
 if __name__ == "__main__":
-    timechunk = "daily"
-    for year in range(2013, 2018):
+    timechunk = "weekly"
+    # for year in range(2013, 2018):
+    for year in range(2017, 2017):
         sumsfolderpath = f"{timechunk}/sums/{year}"
         totals = pd.DataFrame()
         for infile in os.listdir(sumsfolderpath):
             colname = infile.split(f"_{year}")[0]
-            totals[colname] = pd.read_csv(infile).sum()
+            totals[colname] = pd.read_csv(f"{sumsfolderpath}/{infile}").sum()
         totals.to_csv(f"{timechunk}/totals/total_{year}.csv")
